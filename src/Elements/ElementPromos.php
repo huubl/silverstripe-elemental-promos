3abeb313b5435b30ecf521479ed8757e19191d1a
Jason Irish <jsirish@users.noreply.github.com>
2019-06-01 18:47:58 -0500

    ElementPromos owns Promos (#21)

--- src/Elements/ElementPromos.php
+++ src/Elements/ElementPromos.php
@@ -71,6 +71,13 @@ class ElementPromos extends BaseElement
     );
 
     /**
+     * @var array
+     */
+    private static $owns = [
+        'Promos',
+    ];
+
+    /**
      * Set to false to prevent an in-line edit form from showing in an elemental area. Instead the element will be
      * clickable and a GridFieldDetailForm will be used.
      *
